8df5302beb56c845d673b4f2cc05035c9f229035
GensengG <168531024+GensengG@users.noreply.github.com>
2024-08-28 23:19:06 +0300

    Правки Задание 5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -99,55 +99,6 @@ class PrintEditionItem {
   }
 
 
-const library = new Library("Библиотека имени Ленина");
-
-library.addBook(
-  new DetectiveBook(
-    "Артур Конан Дойл",
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
-  )
-);
-
-library.addBook(
-  new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
-  )
-);
-
-library.addBook(
-    new NovelBook(
-        "Герберт Уэллс", 
-        "Машина времени", 
-        1895, 
-        138
-    )
-);
-
-library.addBook(
-    new Magazine(
-        "Мурзилка", 
-        1924, 
-        60
-    )
-);
-console.log(library)
-
-console.log(library.findBookBy("name", "Пикник на обочине")); //null
-console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
-
- console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-library.giveBookByName("Машина времени");
-console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
-
-
-
-
-
 class Student {
     constructor(name, gender, age) {
         this.name = name;

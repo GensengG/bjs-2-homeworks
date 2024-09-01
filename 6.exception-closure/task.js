5f9f35aa28a163221d02d6fbfd2ec35591ed3db5
GensengG <168531024+GensengG@users.noreply.github.com>
2024-09-01 14:38:10 +0300

    Шестое задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+function parseCount(number) {
+    if (Number.isNaN(Number.parseFloat(number))) {
+        throw new Error ("Невалидное значение");
+    } else {
+        return Number.parseFloat(number);
+    }
+}
+
+function validateCount(number) {
+    try {
+       return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c){
+        if ((a + b) < c || (a + c < b) || (b + c < a)){
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+    get area(){
+        let p = (this.a + this.b + this.c) / 2;
+        let S = p * (p - this.a) * (p - this.b) * (p - this.c);
+        return Math.sqrt(S).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        class WarTriangle {
+            get perimeter(){
+                return "Ошибка! Треугольник не существует";
+            }
+            get area(){
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+        return new WarTriangle;
+    }
+}﻿
